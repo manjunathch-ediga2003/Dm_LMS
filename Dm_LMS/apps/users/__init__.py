b2b9308729d63cc1c8af4de59ea1e21ebe0e7f47
manjunathch-ediga2003 <manjunathchk456@gmail.com>
2026-04-08 21:28:12 +0530

    Delete Dm_LMS directory

--- Dm_LMS/apps/users/__init__.py
+++
@@ -1,1 +0,0 @@
-# Directory for user-related templates, place an empty __init__.py here to treat it as a package
--- Dm_LMS/apps/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=150)
-    email = models.EmailField()
-    password = models.CharField(max_length=128)
-
-    def __str__(self):
-        return self.username
